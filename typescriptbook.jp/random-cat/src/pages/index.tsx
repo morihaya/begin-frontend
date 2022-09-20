b93dff489182bb0284110b449d1f16d68941f986
morihaya <yukiya.hayashi@gmail.com>
2022-09-20 18:10:07 +0900

    feat: handleClick

--- typescriptbook.jp/random-cat/src/pages/index.tsx
+++ typescriptbook.jp/random-cat/src/pages/index.tsx
@@ -1,18 +1,5 @@
 import { useState } from "react";
 
-const catImages: string[] =[
-    "https://cdn2.thecatapi.com/images/bpc.jpg",
-    "https://cdn2.thecatapi.com/images/eac.jpg",
-    "https://cdn2.thecatapi.com/images/6qi.jpg",
-];
-
-const randomCatImage = (): string => {
-    const index = Math.floor(Math.random() * catImages.length);
-    return catImages[index]
-};
-
-
-
 interface CatCategory {
     id: number;
     name: string;
@@ -33,18 +20,14 @@ const fetchCatImage = async (): Promise<SearchCatImage> => {
     return result[0];
 };
 
-fetchCatImage().then((image) => {
-    console.log(`猫の画像: ${image.url}`);
-});
-
-
 const IndexPage = () => {
     const [catImageUrl, setCatImageUrl] = useState(
     "https://cdn2.thecatapi.com/images/bpc.jpg"
     );
 
-    const handleClick = () => {
-        setCatImageUrl(randomCatImage());
+    const handleClick = async() => {
+        const image = await fetchCatImage();
+        setCatImageUrl(image.url);
     };
 
     return (
