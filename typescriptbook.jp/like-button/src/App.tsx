f45bcc3d474cacb503fc4fb1a31b8e19b8d4244c
morihaya <yukiya.hayashi@gmail.com>
2022-09-20 15:24:11 +0900

    Add: LikeButton() for prototype

--- typescriptbook.jp/like-button/src/App.tsx
+++ typescriptbook.jp/like-button/src/App.tsx
@@ -5,9 +5,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">TypeScriptはいいぞ</header>
+      <header className="App-header">
+        <LikeButton />
+      </header>
     </div>
   );
+}
+
+function LikeButton() {
+  return <span>いいねボタン予定地</span>
 }
 
 export default App;
